8898c10b24ff9dc4267976de7cbc4913c574ebf4
czm41k <etselikov@express42.com>
2021-11-08 00:05:42 +0300

    docker-compuse up works well

--- code/script.py
+++ code/script.py
@@ -111,27 +111,27 @@ def load_test(summary: dict, host_header: str, ui: bool, users:int=1, time:int()
 # class MyEnv(Environment):
 
 
-class UserBehaviour(SequentialTaskSet):
-  def on_start(self):
-    self.client.verify = True
+# class UserBehaviour(SequentialTaskSet):
+#   def on_start(self):
+#     self.client.verify = True
 
-  @task
-  def my_task(self):
-      with self.client.get("/") as get_resp:
-        # print(f"{get_resp.headers=}")
-        print(get_resp)
-        # print(get_resp.text)
-        print(get_resp.url)
+#   @task
+#   def my_task(self):
+#       with self.client.get("/") as get_resp:
+#         # print(f"{get_resp.headers=}")
+#         print(get_resp)
+#         # print(get_resp.text)
+#         print(get_resp.url)
 
 
-class User(HttpUser):
-  tasks = [UserBehaviour]
-  wait_time = between(1,2)
+# class User(HttpUser):
+#   tasks = [UserBehaviour]
+#   wait_time = between(1,2)
 
-  # def __init__(self, *args, **kwargs):
-  #   HttpUser.__init__(self, *args, **kwargs)
-  #   if 'host-header' in kwargs:
-  #     self.host_header = kwargs['host_header']
+#   # def __init__(self, *args, **kwargs):
+#   #   HttpUser.__init__(self, *args, **kwargs)
+#   #   if 'host-header' in kwargs:
+#   #     self.host_header = kwargs['host_header']
 
 
 
